1f27e46b24c212a49717deb078eba2aa785c3ed0
Murad501 <murad.hossain5501@gmail.com>
2026-06-06 23:00:07 +0600

    docs: update README with project architecture and set production API base URL

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,8 @@ import { store } from "../store";
 import { logout } from "../store/authSlice";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
+  baseURL: "https://project-management-server-sooty.vercel.app/api/v1",
   withCredentials: true,
 });
 
